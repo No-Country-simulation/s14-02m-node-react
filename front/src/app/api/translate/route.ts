92984b592b3959e742e28050111f51ade4e99ea7
Felipe <felipe.calderon321@gmail.com>
2024-04-01 10:58:32 -0300

    fix-gpt-folder-error-and-parse-response

---
+++ front/src/app/api/translate/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.text()
+  const [messageUnparse, toUnparse] = body.split('&')
+  const [_key, message] = messageUnparse.split('=')
+  const [__key, to] = toUnparse.split('=')
+  const model = process.env.GPT_MODEL
+  const apikey = process.env.GPT_KEY
+  
+  const messages = [
+    {
+      "role": "system",
+      "content": `Traduce la frase del usuario al idioma ${to} según formato iso-639-1, debes responder en formato JSON con propiedades "from" que sería el formato iso-639-1 del idioma original escrito por el usuario, además una segunda propiedad "translated" que sería el idioma traducido.`
+    },
+    {
+      "role": "user",
+      "content": message
+    }
+  ]
+    
+    const fetchgpt = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apikey}`
+        },
+        body: JSON.stringify({messages, model, response_format: { "type": "json_object" }})
+      })
+      const response = await fetchgpt.json()
+      const parseGPT = JSON.parse(response.choices[0].message.content)
+      return NextResponse.json(parseGPT)
+    }
