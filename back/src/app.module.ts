92203be560613402b179fa21f028215ad5d6d92e
Facurro <facundo.chiabotto@gmail.com>
2024-04-04 10:22:10 -0300

    feat-02-endpoint translate

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TranslationController } from './app.controller';
+import { translateService } from './app.service'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TranslationController],
+  providers: [AppService, translateService],
 })
 export class AppModule {}
