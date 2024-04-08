4c2b25166d6f4d10717432dfe7ee59465eff9f4c
lucas id betan <lucas.idbetan@gmail.com>
2024-04-08 12:14:53 -0300

    feat-se hizo pull de la config de prettier

--- front/src/components/responseGPT.tsx
+++ front/src/components/responseGPT.tsx
@@ -1,11 +1,11 @@
 import { IresponseGPT } from "@/interfaces/gpt.interface";
 
 export default function ResponseGPT({ response }: { response: IresponseGPT }) {
-  return (
-    <>
-      Respuesta ChatGPT: {response.translated}
-      <br></br>
-      Idioma: {response.from}
-    </>
-  )
+	return (
+		<>
+			Respuesta ChatGPT: {response.translated}
+			<br></br>
+			Idioma: {response.from}
+		</>
+	);
 }
