634a091b8f824d5cb27160df1376774f84e2ef29
JordanES <jordan11.tauro@gmail.com>
2024-04-09 12:24:39 -0500

    limitar texto a 200 caracteres antes de convertir a audio

--- back/src/gpt/gpt.controller.ts
+++ back/src/gpt/gpt.controller.ts
@@ -12,31 +12,37 @@ export class GptController {
     const jsonRes = JSON.parse(await this.gptService.translate(requestBody));
     return jsonRes;
   }
+
   @Post('/translate-to-audio')
-  async translateToAudio(@Body('text') text: string, @Res() response: Response) {
-      try {
-          if (text.trim() === '') {
-              // Si el texto está vacío, envía un mensaje de error con el código de estado 400
-              response.status(HttpStatus.BAD_REQUEST).send({ message: 'El texto proporcionado está vacío. Por favor, proporcione un texto válido.' });
-              return;
-          }
-          const result = await this.gptService.convertToAudio(text);
-  
-          if (result.success) {
-            const uniqueFilename = `translated_audio_${Date.now()}.mp3`;
-              // Configura los encabezados de la respuesta HTTP para enviar el archivo de audio
-              response.set('Content-Type', 'audio/mpeg');
-              response.set('Content-Disposition', `attachment; filename="${uniqueFilename}"`);
-  
-              // Envía el contenido del archivo de audio al cliente como respuesta
-              response.send(result.audioContent);
-          } else {
-              response.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: result.message });
-          }
-      } catch (error) {
-          response.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: 'Error al procesar la solicitud' });
-      }
-  } 
+    async translateToAudio(@Body('text') text: string, @Res() response: Response) {
+        try {
+            if (text.trim() === '') {
+                response.status(HttpStatus.BAD_REQUEST).send({ message: 'El texto proporcionado está vacío. Por favor, proporcione un texto válido.' });
+                return;
+            }
+            
+            if (text.length > 200) {
+                response.status(HttpStatus.BAD_REQUEST).send({ message: 'El texto proporcionado excede el límite de 200 caracteres.' });
+                return;
+            }
+
+            const result = await this.gptService.convertToAudio(text);
+
+            if (result.success) {
+                const uniqueFilename = `translated_audio_${Date.now()}.mp3`;
+                // Configura los encabezados de la respuesta HTTP para enviar el archivo de audio
+                response.set('Content-Type', 'audio/mpeg');
+                response.set('Content-Disposition', `attachment; filename="${uniqueFilename}"`);
+
+                // Envía el contenido del archivo de audio al cliente como respuesta
+                response.send(result.audioContent);
+            } else {
+                response.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: result.message });
+            }
+        } catch (error) {
+            response.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: 'Error al procesar la solicitud' });
+        }
+    }
 
   @Get('/test')
   test() {
