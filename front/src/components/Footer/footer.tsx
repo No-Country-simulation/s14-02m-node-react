bcae41fe7ecc0c3cbcc5d2c6ad736026c4e08189
facundocont <facundocont.fc@gmail.com>
2024-04-09 13:31:26 -0300

    Update footer.tsx

--- front/src/components/Footer/footer.tsx
+++ front/src/components/Footer/footer.tsx
@@ -1,10 +1,12 @@
+import CopyrightIcon from "@/components/Footer/icons/CopyrightIcon";
+
 interface FooterProps {
-	iconItems: { items: footerItem[] };
+	iconItems: footerItem[];
 	appName: string;
 }
 
 interface footerItem {
-	icon: string;
+	icon: (props: any) => JSX.Element;
 	label: string;
 	link: string;
 	isValid: boolean;
@@ -13,26 +15,37 @@ interface footerItem {
 export default function Footer({ iconItems, appName }: FooterProps) {
 	const currentYear = new Date().getFullYear();
 
-	const iconItemsFiltered: footerItem[] = iconItems.items.filter(
+	const iconItemsFiltered: footerItem[] = iconItems.filter(
 		(icon: footerItem) => icon.isValid
 	);
 
 	return (
 		<>
-			<footer>
-				<div>
-					{iconItemsFiltered.map((icon: footerItem) => (
-						<a href={icon.link}>
-							<img src={icon.icon} alt={icon.label} />
-							<span>{icon.label}</span>
-						</a>
-					))}
-				</div>
-				<div className="">
-					<span>icon c</span>
-					<span>{appName}</span>
-					<span>{currentYear}</span>
-					<span>Todos los derechos reservados.</span>
+			<footer className="flex justify-center h-32">
+				<div className="flex flex-col border-t-1 border-t-[#9CA3AF]/40 w-4/5 justify-evenly items-center">
+					<div className="flex flex-row gap-6">
+						{iconItemsFiltered.map((icon: footerItem, index) => (
+							<a
+								key={index}
+								href={icon.link}
+								className="hover:scale-125 inline-block"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{icon.icon({ fillColor: "#9CA3AF", width: 24, height: 24 })}
+							</a>
+						))}
+					</div>
+					<div className="flex flex-wrap text-[#9CA3AF] text-base gap-1 justify-center items-center">
+						<div className="flex flex-row">
+                            <span>
+                                <CopyrightIcon fillColor="#9CA3AF" width={24} height={24} />
+                            </span>
+                            <span>{currentYear}</span>
+                            <span>{appName}.</span>
+                        </div>
+						<span>Todos los derechos reservados.</span>
+					</div>
 				</div>
 			</footer>
 		</>
