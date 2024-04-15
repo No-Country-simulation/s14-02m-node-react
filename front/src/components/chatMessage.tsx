9b4010c123e93f04888278289055349b4375603d
Pablo Gutiérrez <gutierrezpabloraul@gmail.com>
2024-04-15 12:30:25 -0300

    message-union

--- front/src/components/chatMessage.tsx
+++ front/src/components/chatMessage.tsx
@@ -5,16 +5,21 @@ import Image from "next/image";
 
 export default function ChatMessage({ chat }: { chat: IHistory }) {
 	const rolFinded = filterRol(chat.rol);
-	switch(rolFinded){
-		case 'Usuario': return <ClientMessageBox chat={chat} />
-		default: return <ResponseMessageBox chat={chat} />
-	}
+	return (
+		<div className="flex flex-row">
+			<div className="w-10/12 bg-secundario">
+				{ rolFinded === 'Usuario' && <ClientMessageBox chat={chat} /> }
+				{ rolFinded === 'Talkiamos' && <ResponseMessageBox chat={chat} /> }
+			</div>
+			<div className="w-2/12 bg-secundario">Aqui van los botones</div>
+		</div>
+	)
 }
 
 const ClientMessageBox = ({ chat }: { chat: IHistory }) => {
 	const languageFinded = filterLanguage(chat.langCode);
 	const langIcon = `https://unpkg.com/language-icons@0.3.0/icons/${languageFinded.to}.svg`
-	return <div key={chat.message} className={`flex flex-row-reverse items-center text-right gap-3 bg-blue-100 rounded-xl py-3 px-6`}>
+	return <div key={chat.message} className={`flex flex-row-reverse items-center text-right gap-3 text-white md:text-black md:bg-blue-100 rounded-xl py-3 px-6`}>
 	<Image src={langIcon} width={30} height={30} alt={`Nombre: ${languageFinded.name}`} />
 	<p>{chat.message}</p>
 </div>
@@ -23,7 +28,7 @@ const ClientMessageBox = ({ chat }: { chat: IHistory }) => {
 const ResponseMessageBox = ({ chat }: { chat: IHistory }) => {
 	const languageFinded = filterLanguage(chat.langCode);
 	const langIcon = `https://unpkg.com/language-icons@0.3.0/icons/${languageFinded.to}.svg`
-	return <div key={chat.message} className={`flex flex-row justify-start items-center text-left gap-3 bg-green-100 rounded-xl py-3 px-6`}>
+	return <div key={chat.message} className={`flex flex-row justify-start items-center text-left gap-3 text-white md:text-black md:bg-green-100 rounded-xl py-3 px-6 mb-3`}>
 	<Image src={langIcon} width={30} height={30} alt={`Nombre: ${languageFinded.name}`} />
 	<p>{chat.message}</p>
 </div>
--- front/src/components/history.tsx
+++ front/src/components/history.tsx
@@ -8,7 +8,7 @@ export default function History() {
 	const { history, cleanHistory } = useHistoryStore();
 	if(history.length > 0) return (
 		<div className="w-full">
-			<div className="grid grid-cols-2 gap-4 my-auto bg-slate-50 py-3 px-3 rounded-xl">
+			<div className="flex flex-col md:grid md:grid-cols-2 gap-4 my-auto bg-slate-50 py-3 px-3 rounded-xl">
 				{/*3. Mapeo una lista con los objetos del array history*/}
 				{ history.map((chat) => <ChatMessage key={chat.message} chat={chat} />) }
 			</div>
