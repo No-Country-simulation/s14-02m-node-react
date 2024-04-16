c880c6cc754ca52f3e75607709940d4f55f8e36f
facundocont <facundocont.fc@gmail.com>
2024-04-16 10:35:02 -0300

    Update page.tsx
    Se muestra el mensaje de bienvenida siempre que no haya historial

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,24 +1,34 @@
+"use client"
 import UserForm from "@/components/Forms/userForm";
 import History from "@/components/history";
+import { useHistoryStore } from "@/stores/historyStore";
 
 export default function Home() {
+	const { history } = useHistoryStore();
+
 	return (
 		<>
-		<div className="h-[calc(100dvh-64px)] flex flex-col justify-between">
-			<div className="text-[#9CA3AF] w-10/12 max-w-80 mx-auto my-auto text-center flex flex-col justify-center gap-10">
-				<h1 className="text-2xl">
-					¡Bienvenido a <br />
-					Talk<span className="text-secundario/90">IA</span>mos!
-				</h1>
-				<h2>Traduce de manera simple y eficaz todo lo que quieras</h2>
-				<p>Esta aplicación no guarda ni registra las traducciones solicitadas si no te encontrás registrado. Para registrarte hace click <a href="#">aquí</a></p>
+			<div className="h-[calc(100dvh-64px)] flex flex-col justify-between">
+				{history.length === 0 ? 
+					<div className="text-gris w-10/12 max-w-80 mx-auto my-auto text-center flex flex-col justify-center gap-10">
+						<h1 className="text-2xl">
+							¡Bienvenido a <br />
+							Talk<span className="text-[#e55958]/90">IA</span>mos!
+						</h1>
+						<h2>Traduce de manera simple y eficaz todo lo que quieras</h2>
+						<p>
+							Esta aplicación no guarda ni registra las traducciones solicitadas si no
+							te encontrás registrado. Para registrarte hace click <a href="#">aquí</a>
+						</p>
+					</div>
+				 : 
+					<History />
+				}
+
+				<div className="flex flex-col justify-center items-center w-11/12 h-screen mx-auto">
+					<UserForm />
+				</div>
 			</div>
-			<div className="flex flex-col justify-center items-center w-11/12 h-screen mx-auto">
-				<History />
-				<UserForm />
-			</div>
-		</div>
-			
 		</>
 	);
 }
