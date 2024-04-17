5cc245f3f5bb5630ba1b104f5a4e2af5db0f4dd9
Esteban Sayago <sayagoo823@gmail.com>
2024-04-17 14:02:54 -0300

    [13] - normalizar errores del servidor

--- back/src/gpt/gpt.controller.ts
+++ back/src/gpt/gpt.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get,Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Get, Res, HttpStatus } from '@nestjs/common';
 import { GptService } from './gpt.service';
 import { Response } from 'express';
 import { Payload } from './interfaces/payload.interface';
@@ -9,34 +9,48 @@ export class GptController {
 
   @Post('/translate')
   async translate(@Body() requestBody: Payload) {
-    const jsonRes = JSON.parse(await this.gptService.translate(requestBody));
-    return jsonRes;
+    const response = JSON.parse(await this.gptService.translate(requestBody));
+    return response;
   }
 
   @Post('/translate-to-audio')
-    async translateToAudio(@Body('text') text: string, @Res() response: Response) {
-        try {
-            if (text.trim() === '') {
-                response.status(HttpStatus.BAD_REQUEST).send({ message: 'El texto proporcionado está vacío. Por favor, proporcione un texto válido.' });
-                return;
-            }
-            
-            if (text.length > 200) {
-                response.status(HttpStatus.BAD_REQUEST).send({ message: 'El texto proporcionado excede el límite de 200 caracteres.' });
-                return;
-            }
+  async translateToAudio(
+    @Body('text') text: string,
+    @Res() response: Response,
+  ) {
+    try {
+      if (text.trim() === '') {
+        response.status(HttpStatus.BAD_REQUEST).send({
+          message:
+            'El texto proporcionado está vacío. Por favor, proporcione un texto válido.',
+        });
+        return;
+      }
 
-            const result = await this.gptService.convertToAudio(text);
+      if (text.length > 200) {
+        response.status(HttpStatus.BAD_REQUEST).send({
+          message: 'El texto proporcionado excede el límite de 200 caracteres.',
+        });
+        return;
+      }
 
-            if (result.success) {
-              response.status(HttpStatus.OK).json({ success: true, audioUrl: result.audioUrl });
-            } else {
-                response.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: result.message });
-            }
-        } catch (error) {
-            response.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ message: 'Error al procesar la solicitud' });
-        }
+      const result = await this.gptService.convertToAudio(text);
+
+      if (result.success) {
+        response
+          .status(HttpStatus.OK)
+          .json({ success: true, audioUrl: result.audioUrl });
+      } else {
+        response
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .send({ message: result.message });
+      }
+    } catch (error) {
+      response
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .send({ message: 'Error al procesar la solicitud' });
     }
+  }
 
   @Get('/test')
   test() {
