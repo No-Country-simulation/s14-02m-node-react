6f7d05da25dec403295853e24b0c432341c78e39
Esteban Sayago <sayagoo823@gmail.com>
2024-04-17 20:11:07 -0300

    [18]-audio

--- front/src/components/playButton.tsx
+++ front/src/components/playButton.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { AudioResponse } from "@/interfaces/backRes.interface";
 import { Button } from "@nextui-org/react";
-import { useState } from "react";
+import { useState,useRef } from "react";
 import { IGroupedMessage } from "@/interfaces/message.interface";
 import { useHistoryStore } from "@/stores/historyStore";
 
 export default function PlayButton({ chat }: { chat: IGroupedMessage }) {
-	const [audioURL, setAudioURL] = useState<null | string>(null);
 	const { history, updateAudio } = useHistoryStore();
+	const[isLoading,setIsLoading] = useState(false);
+	const audioRef = useRef<HTMLAudioElement>(null);
 
 	const handlePlay = async () => {
 		try {
+			setIsLoading(true)
 			const response = await fetch("/api/audio", {
 				method: "POST",
 				headers: {
@@ -22,22 +24,31 @@ export default function PlayButton({ chat }: { chat: IGroupedMessage }) {
 				const audioResponse: AudioResponse = await response.json();
 				//Actualiza el audioStore con el id y la url de la respuesta de voz.
 				updateAudio(chat, audioResponse.audioUrl);
+				setIsLoading(false)
 				console.log("Response OK", audioResponse.audioUrl);
 			}
 		} catch (error) {
 			console.log("Error", error);
 		}
 	};
+
+	const play = ()=>{
+		audioRef.current?.play();
+	}
 	console.log(history);
 	return (
 		<>
+
+		{chat.audioUrl ? 
+		<Button onClick={play}>
+			🔊 Escuchar
+			<audio ref={audioRef} src={chat.audioUrl}></audio>
+		</Button>
+		:
 			<Button className="w-1/2" isIconOnly={true} onClick={handlePlay}>
-				{chat.audioUrl ? (
-					<audio className="w-full" controls src={chat.audioUrl}></audio>
-				) : (
-					"🔊"
-				)}
+			🔊 {isLoading && "Cargando..."}
 			</Button>
+	}
 		</>
 	);
 }
